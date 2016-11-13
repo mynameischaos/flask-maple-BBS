fb891874d9f12666945eb8920891961dff43f70a
钟华松 <zhonghsuestc@163.com>
2016-11-13 10:13:45 +0800

    add all

--- run.py
+++ run.py
@@ -16,9 +16,12 @@ if __name__ == '__main__':
 
 
 
+
+
+# run redis before run postgresql server
+# redis-server
+
 # run postgresql server
 # pg_ctl - D / usr / local / var / postgres - l / usr / local / var / postgres / server.log start
 # stop server
 # pg_ctl -D /usr/local/var/postgres stop -s -m fast
-# run redis
-# redis-server
